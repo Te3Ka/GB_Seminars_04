fee8f61573b165d162bcdbdd3a3f6754ff9834a8
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-01-29 11:42:05 +0300

    Задание 1. Выполнено

--- Exercise01/Program.cs
+++ Exercise01/Program.cs
@@ -19,3 +19,26 @@ void Author()
     Console.WriteLine();
 }
 
+int SumNumbers(int num)
+{
+    if (num < 1)
+        return -1;
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+Console.WriteLine("Программа выводит на экран сумму чисел от 1 до введённого целого положительного числа");
+Console.Write("Введите число: ");
+int.TryParse(Console.ReadLine(), out int A);
+while (A <= 0)
+{
+    Console.Write("Введено число меньше 1, повторите ввод: ");
+    int.TryParse(Console.ReadLine(), out A);
+}
+Console.WriteLine($"Сумма чисел от 1 до {A} = {SumNumbers(A)}");
+
+Author();
